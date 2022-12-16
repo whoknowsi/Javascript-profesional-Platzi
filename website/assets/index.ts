6fa997a34ff2503258b7c570763b0d2306b8b66b
whoknowsi <echosmania@gmail.com>
2022-12-16 19:33:18 -0300

    Change index.js to ts

--- website/assets/index.ts
+++ website/assets/index.ts
@@ -4,13 +4,13 @@ import AutoPause from "@whomi/mediaplayer-platzi/lib/plugins/AutoPause"
 import AdsPuglin from "@whomi/mediaplayer-platzi/lib/plugins/Ads/"
 
 const video = document.querySelector('.movie')
-const buttonToggleReproduce = document.querySelector('#toggle-reproduce')
-const buttonToggleMute = document.querySelector('#toggle-mute')
+const buttonToggleReproduce: HTMLElement | null = document.querySelector('#toggle-reproduce')
+const buttonToggleMute: HTMLElement | null = document.querySelector('#toggle-mute')
 
 const player = new MediaPlayer({ el: video, plugins: [new AutoPlay(), new AutoPause(), new AdsPuglin()] })
 
-buttonToggleReproduce.addEventListener('click', () => player.togglePlay())
-buttonToggleMute.addEventListener('click', () => player.toggleMute())
+buttonToggleReproduce?.addEventListener('click', () => player.togglePlay())
+buttonToggleMute?.addEventListener('click', () => player.toggleMute())
 
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register(new URL('../sw.js', import.meta.url)).catch(error => {
